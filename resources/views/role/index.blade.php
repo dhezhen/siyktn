52300ef4dcf4feea9f3286f310a04fd00e5b6cd0
dhezhen <adehusen@gmail.com>
2026-09-06 23:52:07 +0700

    feat: create index view for role management with permission and user counts

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -40,7 +40,7 @@
                             </td>
                             <td class="px-5 py-3 text-slate-700">{{ $role->users_count }}</td>
                             <td class="px-5 py-3">
-                                <div class="flex items-center justify-end gap-0.5">
+                                <div class="flex items-center justify-end gap-2">
                                     @if ($isSuper)
                                         <span class="text-xs text-slate-400">Bawaan sistem</span>
                                     @else
